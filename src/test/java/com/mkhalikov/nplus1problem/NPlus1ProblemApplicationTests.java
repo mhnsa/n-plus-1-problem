99e446ce9130c41c7bd8b1a5286c8512748cde85
marselkhalikov <mhnsa@mail.ru>
2023-02-03 16:16:47 +0300

    Initial commit

---
+++ src/test/java/com/mkhalikov/nplus1problem/NPlus1ProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkhalikov.nplus1problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NPlus1ProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
